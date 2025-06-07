3b8ca5d2d653a2fe31e36fb624eecc7a23a46cb8
nyangshawbin <nyangbin@gmail.com>
2025-06-07 10:52:27 +0800

    feat: abstract create note button

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -7,11 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:ai_voice_note/theme/brand_text_styles.dart';
 import 'package:ai_voice_note/theme/brand_spacing.dart';
-import 'package:ai_voice_note/features/note/application/note_controller.dart';
-import 'package:ai_voice_note/features/note/presentation/note_editor_page.dart';
-import 'package:ai_voice_note/features/shared/elements.dart';
-import 'package:ai_voice_note/theme/brand_radius.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ai_voice_note/features/note/presentation/create_note_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -38,61 +34,7 @@ class _HomePageState extends State<HomePage> {
               },
               showToggle: true,
               showBackIcon: false,
-              rightActions: [
-                Consumer(
-                  builder: (context, ref, child) => Container(
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: BrandSpacing.md,
-                          vertical: BrandSpacing.md,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BrandRadius.medium,
-                        ),
-                        iconColor: BrandColors.backgroundDark,
-                      ),
-                      onPressed: () async {
-                        try {
-                          final newNote = await ref
-                              .read(noteControllerProvider)
-                              .create();
-
-                          if (newNote != null) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    NoteEditorPage(noteId: newNote.id),
-                              ),
-                            );
-                          } else {
-                            CustomSnackBar.show(
-                              context,
-                              message: 'Failed to create note.',
-                              backgroundColor: BrandColors.warning,
-                              textStyle: BrandTextStyles.small,
-                            );
-                          }
-                        } catch (e) {
-                          CustomSnackBar.show(
-                            context,
-                            message: 'An error occurred: $e',
-                            backgroundColor: BrandColors.warning,
-                            textStyle: BrandTextStyles.small,
-                          );
-                        }
-                      },
-                      child: Text(
-                        'Create Note',
-                        style: BrandTextStyles.small.copyWith(
-                          color: BrandColors.textDark,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+              rightActions: const [CreateNoteButton()],
             ),
           ),
           Expanded(
---
+++ lib/features/note/presentation/create_note_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ai_voice_note/theme/brand_colors.dart';
+import 'package:ai_voice_note/theme/brand_text_styles.dart';
+import 'package:ai_voice_note/theme/brand_spacing.dart';
+import 'package:ai_voice_note/theme/brand_radius.dart';
+import 'package:ai_voice_note/features/note/application/note_controller.dart';
+import 'package:ai_voice_note/features/note/presentation/note_editor_page.dart';
+import 'package:ai_voice_note/features/shared/elements.dart';
+
+class CreateNoteButton extends ConsumerWidget {
+  const CreateNoteButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: BrandSpacing.md,
+          vertical: BrandSpacing.md,
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BrandRadius.medium),
+        iconColor: BrandColors.backgroundDark,
+      ),
+      onPressed: () async {
+        try {
+          final newNote = await ref.read(noteControllerProvider).create();
+
+          if (newNote != null) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NoteEditorPage(noteId: newNote.id),
+              ),
+            );
+          } else {
+            CustomSnackBar.show(
+              context,
+              message: 'Failed to create note.',
+              backgroundColor: BrandColors.warning,
+              textStyle: BrandTextStyles.small,
+            );
+          }
+        } catch (e) {
+          CustomSnackBar.show(
+            context,
+            message: 'An error occurred: $e',
+            backgroundColor: BrandColors.warning,
+            textStyle: BrandTextStyles.small,
+          );
+        }
+      },
+      child: Text(
+        'Create Note',
+        style: BrandTextStyles.small.copyWith(color: BrandColors.textDark),
+      ),
+    );
+  }
+}
